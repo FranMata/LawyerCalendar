975b8abf06c2c78a573c80e65fec4a06c06ee795
Francisco Mata <francisco.redondo23@gamil.com>
2022-08-11 10:15:47 -0600

    ef configuration done

---
+++ LawyerCalendar/LawyerCalendar/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace LawyerCalendar.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            Appointments = new HashSet<Appointment>();
+        }
+
+        public int Id { get; set; }
+        public int? IdDocument { get; set; }
+        public string BirthDate { get; set; }
+        public string Name { get; set; }
+        public int? PaymentMethodId { get; set; }
+        public string PaymentMethodData { get; set; }
+
+        public virtual PaymentMethod PaymentMethod { get; set; }
+        public virtual ICollection<Appointment> Appointments { get; set; }
+    }
+}
