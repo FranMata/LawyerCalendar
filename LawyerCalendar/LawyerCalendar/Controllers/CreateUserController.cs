834e40717b4e3518cfe1ca1119fbf71384722fa6
Francisco Mata <francisco.redondo23@gamil.com>
2022-08-11 10:58:55 -0600

    create user done

---
+++ LawyerCalendar/LawyerCalendar/Controllers/CreateUserController.cs
@@ -0,0 +1,44 @@
+﻿using LawyerCalendar.Models;
+using LawyerCalendar.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LawyerCalendar.Controllers
+{
+    public class CreateUserController : Controller
+    {
+        private readonly LawyerCalendarContext _context;
+
+        public CreateUserController(LawyerCalendarContext context) => _context = context;
+
+        public IActionResult Index()
+        {
+            ViewData["PaymentMethods"] = new SelectList(_context.PaymentMethods, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(UserViewModel user)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            User userEF = new User()
+            {
+                IdDocument = user.IdDocument,
+                BirthDate = user.BirthDate.ToShortDateString(),
+                Name = user.Name,
+                PaymentMethodId = user.PaymentMethodId,
+                PaymentMethodData = user.PaymentMethodData
+            };
+
+            _context.Add(userEF);
+            await _context.SaveChangesAsync();
+            return Redirect(@"/");
+        }
+    }
+}
---
+++ LawyerCalendar/LawyerCalendar/Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LawyerCalendar.Models.ViewModels
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Cedula")]
+        public int IdDocument { get; set; }
+
+        [Required]
+        [Display(Name = "Fecha de nacimiento")]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Metodo de pago")]
+        public int PaymentMethodId { get; set; }
+
+        [Required]
+        [Display(Name = "Informacion del metodo de pago")]
+        public string PaymentMethodData { get; set; }
+    }
+}
